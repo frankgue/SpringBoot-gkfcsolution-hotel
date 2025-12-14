8c7423917d82356b1dbd243f571ddddc265ae7d8
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2025-12-14 03:32:28 +0100

    first commit

---
+++ src/main/java/com/gkfcsolution/gkfcsolutionhotel/GkfcsolutionHotelApplication.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.gkfcsolutionhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkfcsolutionHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GkfcsolutionHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gkfcsolution/gkfcsolutionhotel/GkfcsolutionHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkfcsolution.gkfcsolutionhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkfcsolutionHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
